69645938c0122131947d9e45552d9ca0b9440a1a
zivilekut <zivilekut@gmail.com>
2024-04-08 15:47:12 +0300

    tes1 counts ads

--- src/test/java/SkelbiuTest.java
+++ src/test/java/SkelbiuTest.java
@@ -1,9 +1,11 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -35,14 +37,27 @@ public class SkelbiuTest {
 
     }
 
-    @Test // 1 page of adds
+    @Test // 1 page of ads (not skelbiu, but autoplius)
     public void test1() {
         _globalDriver.get("https://www.skelbiu.lt/skelbimai/?autocompleted=1&keywords=traktorius+22kw&cost_min=&cost_max=&type=0&condition=&cities=0&distance=0&mainCity=0&search=1&category_id=1&user_type=0&ad_since_min=0&ad_since_max=0&visited_page=1&orderBy=-1&detailsSearch=0");
         _globalDriver.findElement(By.id("onetrust-reject-all-handler")).click();
 
-        int totalAds = Integer.parseInt(_globalDriver.findElement(By.xpath("//*[@id=\"body-container\"]/div[2]/div[1]/ul/li[1]/span")).getText());
-        System.out.println(totalAds);
+        snoozeUntilPresence(By.xpath("//*[@id=\"body-container\"]/div[2]/div[1]/ul/li[1]/span"));
+        String totalAds = _globalDriver.findElement(By.xpath("//*[@id=\"body-container\"]/div[2]/div[1]/ul/li[1]/span")).getText();
+        String numericPart = totalAds.replaceAll("[^\\d]", "");
+        int numberOfAds = Integer.parseInt(numericPart);
 
+        int adCounter = 0;
+        for (int i = 1; i < 10; i++) {
+            try {
+                _globalDriver.findElement(By.xpath("//*[@id=\"items-list-container\"]/div[2]/div[" + i + "]"));
+                adCounter++;
+            } catch (NoSuchElementException e) {
+            }
+        }
+        Assert.assertEquals(adCounter, numberOfAds);
+
+        _globalDriver.quit();
     }
 
 }
