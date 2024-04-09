a15cb9fa8386781a924a6dc3897d7ef52d352b50
zivilekut <zivilekut@gmail.com>
2024-04-09 11:44:23 +0300

    test3 done

--- src/test/java/SkelbiuTest.java
+++ src/test/java/SkelbiuTest.java
@@ -57,7 +57,7 @@ public class SkelbiuTest {
         _globalDriver.quit();
     }
 
-    @Test // 2 pages of ads
+    @Test // 2 pages of ads (but could work on the bigger search)
     public void test2() {
         _globalDriver.get("https://www.skelbiu.lt/skelbimai/1?keywords=samotines+plytos");
         _globalDriver.findElement(By.id("onetrust-reject-all-handler")).click();
@@ -97,5 +97,60 @@ public class SkelbiuTest {
         System.out.println("Number of ads: " + adCounter);
         Assert.assertEquals(adCounter, numberOfAds);
         _globalDriver.quit();
+    }
+
+    @Test // Search with more than 200 pages
+    public void test3() {
+        _globalDriver.get("https://www.skelbiu.lt/skelbimai/1?keywords=telefonas");
+        _globalDriver.findElement(By.id("onetrust-reject-all-handler")).click();
+
+        snoozeUntilPresence(By.xpath("//*[@id=\"body-container\"]/div[2]/div[1]/ul/li/span"));
+        String totalAds = _globalDriver.findElement(By.xpath("//*[@id=\"body-container\"]/div[2]/div[1]/ul/li/span")).getText();
+        String numericPart = totalAds.replaceAll("[^\\d]", "");
+        int numberOfAds = Integer.parseInt(numericPart);
+        int adCounter = 0;
+
+        for (int i = 1; i < 202; i++) {
+
+            String url = "https://www.skelbiu.lt/skelbimai/" + i + "?keywords=telefonas";
+            _globalDriver.get(url);
+            if (!_globalDriver.getCurrentUrl().equals(url)) {
+                break; // Exit the loop since we've reached the last page
+            }
+            if (_globalDriver.getCurrentUrl().equals("https://www.skelbiu.lt/skelbimai/201?keywords=telefonas")) {
+                System.out.println("Pagal Jūsų paieškos kriterijus radome labai daug skelbimų.");
+                WebElement limitText = _globalDriver.findElement(By.id("NotFoundHeader"));
+                Assert.assertEquals(limitText.getText(), "Pagal Jūsų paieškos kriterijus radome labai daug skelbimų.");
+                _globalDriver.quit();
+                break;
+            }
+
+            for (int y = 1; y <= 27; y++) {
+                try {
+                    _globalDriver.findElement(By.xpath("(//*[@id='items-list-container']/div[" + (i + 1) + "]/div)[" + y + "]")).click();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    String adID = _globalDriver.findElement(By.cssSelector("#contentArea > div.main-item-container > div.itemscope > div.left-content > div.actions-container > div.block.id")).getText();
+                    if (adID.contains("ID")) {
+                        adCounter++;
+                        System.out.println(adID);
+                    }
+                    _globalDriver.get("https://www.skelbiu.lt/skelbimai/" + i + "?keywords=telefonas");
+                } catch (NoSuchElementException e) {
+                }
+            }
+        }
+        System.out.println("Number of ads: " + adCounter);
+        Assert.assertEquals(adCounter, numberOfAds);
+        _globalDriver.quit();
+    }
+
+    @Test // search with no results
+    public void test4() {
+        _globalDriver.get("https://www.skelbiu.lt/skelbimai/1?keywords=samotines+plytos");
+        _globalDriver.findElement(By.id("onetrust-reject-all-handler")).click();
     }
 }
